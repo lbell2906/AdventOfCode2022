a89ade4029c2f9ce76b86c4a0bf391f52970ad78
Lauren Bell <laurenbell2906@gmail.com>
2022-12-02 22:40:01 +0000

    day 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,23 +1,23 @@
 import { Day } from "../day";
 
 interface MoveObject {
-    roundScore: number;
-    drawValue: string;
-    winValue: string;
+    roundScore: number
+    drawValue: string
+    winValue: string
     looseValue: string
 }
 
 interface roundObject {
-    [key: string] : {yourMove: string, moveScore: number, roundScore: number}
+    [key: string] : {[key: string] : number}
 }
-
 
 class Day2 extends Day {
 
     constructor(){
         super(2);
     }
-
+ // --------------------------- PART 1 -------------------------------
+    // A X rock, B Y paper C Z Scissors
     moves: {[key: string] : MoveObject} = {
         A: {roundScore: 1, drawValue: 'X', winValue: 'Z', looseValue: 'Y'},
         B: {roundScore: 2, drawValue: 'Y', winValue: 'X', looseValue: 'Z'},
@@ -27,76 +27,57 @@ class Day2 extends Day {
         Z: {roundScore: 3, drawValue: 'C', winValue: 'B', looseValue: 'A'}
     }
 
-    roundScores: {[key: string]: number} = {you: 6, them: 0, noone: 3}
+    roundScores: {[key: string]: number} = {you: 6, them: 0, none: 3}
     
 
     findRoundResult = (theirMove: string, yourMove: string): string => {
-        if (this.moves[theirMove].winValue === yourMove) return 'them';
-        if (this.moves[theirMove].looseValue === yourMove) return 'you';
-        if (this.moves[theirMove].drawValue === yourMove) return 'noone';
+        if (this.moves[theirMove].winValue === yourMove) return 'them'
+        if (this.moves[theirMove].looseValue === yourMove) return 'you'
+        if (this.moves[theirMove].drawValue === yourMove) return 'none'
         else return 'Invalid Move';
     }
 
-    getRoundScore = (moves: string, yours: boolean): number => {
-        let theirMove, yourMove
-        if (yours) {[theirMove, yourMove]  = moves.split(' ')}
-        else {[yourMove, theirMove]  = moves.split(' ')}
+    getYourRoundScore = (moves: string): number => {
+        const [theirMove, yourMove]  = moves.split(' ')
         const objectScore = this.moves[yourMove].roundScore
         const roundResult = this.findRoundResult(theirMove, yourMove)
         const resultScore = this.roundScores[roundResult]
         return objectScore + resultScore;
     }
 
-    splitByBlankLine = (input: string): Array<string> => input.split('\n');
-
-    theirTotalScore = 0
-    yourTotalScore = 0
+    splitByBlankLine = (input: string): Array<string> => input.split('\n')
 
     solveForPartOne(input: string): string {
-        const data = this.splitByBlankLine(input);
+        const data = this.splitByBlankLine(input)
+        let yourTotalScore: number = 0
         data.forEach((move) => {
-            this.yourTotalScore += this.getRoundScore(move, true);
-            this.theirTotalScore += this.getRoundScore(move, false);
+            yourTotalScore += this.getYourRoundScore(move)
         })
-        return this.yourTotalScore.toString();
+        return yourTotalScore.toString()
     }
-// X you loose, Y draw, Z you win
-// A rock, B paper C Scissors
-    movesPart2: {[key: string]: roundObject} = {
-       A: {
-        X: {yourMove: 'C', moveScore: 3, roundScore: 0},
-        Y: {yourMove: 'A', moveScore: 1, roundScore: 3},
-        Z: {yourMove: 'B', moveScore: 2, roundScore: 6}
-       },
-       B: {
-        X: {yourMove: 'A', moveScore: 1, roundScore: 0},
-        Y: {yourMove: 'B', moveScore: 2, roundScore: 3},
-        Z: {yourMove: 'C', moveScore: 3, roundScore: 6}
-       },
-       C: {
-        X: {yourMove: 'B', moveScore: 2, roundScore: 0},
-        Y: {yourMove: 'C', moveScore: 3, roundScore: 3},
-        Z: {yourMove: 'A', moveScore: 1, roundScore: 6}
-       }
-   
-    }
-    getRoundScorePt2 = (roundInfo: string): number => {
-        const [theirMove, outcome]  = roundInfo.split(' ')
-        const yourMoveScore = this.movesPart2[theirMove][outcome].moveScore;
-        const yourRoundScore = this.movesPart2[theirMove][outcome].roundScore;
-        return yourMoveScore + yourRoundScore;
+    // --------------------------- PART 2 -------------------------------
+    // X you loose, Y draw, Z you win
+    // A rock, B paper C Scissors
+    movesPart2: roundObject = {
+       A: { X: 3, Y: 4, Z: 8 },
+       B: { X: 1, Y: 5, Z: 9 },
+       C: { X: 2, Y: 6, Z: 7 }
     }
 
-    myTotalScorePt2 = 0
+    getRoundScorePt2 = (roundInfo: string): number => {
+        const [theirMove, outcome] = roundInfo.split(' ')
+        return this.movesPart2[theirMove][outcome]
+    }
+
+    myTotalScorePt2: number = 0
 
     solveForPartTwo(input: string): string {
-        const data = this.splitByBlankLine(input);
+        const data = this.splitByBlankLine(input)
         data.forEach((move) => {
             this.myTotalScorePt2 += this.getRoundScorePt2(move)
         })
-
-        return this.myTotalScorePt2.toString();
+        return this.myTotalScorePt2.toString()
     }
 }
 
-export default new Day2;
+export default new Day2
