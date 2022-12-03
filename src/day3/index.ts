5a7c02e70eb1d537d0bf7750096259d9bd60e4e9
Lauren Bell <laurenbell2906@gmail.com>
2022-12-03 21:37:51 +0000

    day3 pt 2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,4 +1,3 @@
-import { SocketAddress } from "net";
 import { Day } from "../day";
 
 class Day3 extends Day {
@@ -42,9 +41,37 @@ class Day3 extends Day {
         return totalScore.toString();
     }
 
+    splitIntoChunk = (inputArray: Array<any>, chunkSize: number): Array<Array<any>> => {
+        const result = inputArray.reduce((resultArray, item, index) => { 
+        const chunkIndex = Math.floor(index/chunkSize)
+
+        if(!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+        resultArray[chunkIndex].push(item)
+        return resultArray
+        }, [])
+        return result
+    }
+
+    findCommonOfThree = (string1: string, string2: string, string3: string ): string | undefined => {
+        const string1Array = Array.from(string1)
+        const string2Matches = string1Array.filter((character) => string2.includes(character)) 
+        const string3Matches = string1Array.filter((character) => string3.includes(character)) 
+        return this.findCommonCharacter(string2Matches.join(''), string3Matches.join('')) 
+    }
 
     solveForPartTwo(input: string): string {
-        return '';
+        const data = this.splitByLine(input)
+        const dataSplit = this.splitIntoChunk(data, 3)
+        let totalBadgeScore = 0
+        dataSplit.forEach((group) => {
+            const badge = this.findCommonOfThree(group[0], group[1], group[2])
+            const badgeScore = badge ? this.findCharacterScore(badge) : 0
+            totalBadgeScore += badgeScore
+        })
+
+        return totalBadgeScore.toString();
     }
 }
 
